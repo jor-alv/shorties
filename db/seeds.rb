4349d485e81bc02818755c8b67cb5d3490b15db3
James Campbell <jcampbell92@icloud.com>
2022-05-17 17:21:21 -0400

    Added avatars to seed and created basic cards for user profile page

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,13 +14,13 @@ Booking.destroy_all
 clones = ["Black-tie", "Business", "Casual", "Party"]
 
 5.times do
-  User.create!(email:Faker::Internet.email, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, password: "123456")
+  User.create!(email:Faker::Internet.email, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, password: "123456", photo_url: "https://i.pravatar.cc/1000")
 end
 
 User.all.each do |user|
   unless user == User.last
     3.times do
-      Shorty.create!(height: rand(150..175), name: "#{clones[rand(0..3)]} #{user.first_name}", price: rand(100..500), user: user)
+      Shorty.create!(height: rand(150..175), name: "#{clones[rand(0..3)]} #{user.first_name}", price: rand(100..500), user: user, photo_url: "https://i.pravatar.cc/1000")
     end
   end
 end
