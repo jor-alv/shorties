1d3b299c4a9326fafacf61aa25c7b17a8898cd7b
Jorge Alvarez <jorge.a.alvarez@protonmail.com>
2022-05-17 10:42:14 -0400

    added all pundit policies

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_16_214104) do
+ActiveRecord::Schema.define(version: 2022_05_16_215333) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.integer "status", default: 0
+    t.bigint "user_id", null: false
+    t.bigint "shorty_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["shorty_id"], name: "index_bookings_on_shorty_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "shorties", force: :cascade do |t|
     t.integer "height"
@@ -41,5 +53,7 @@ ActiveRecord::Schema.define(version: 2022_05_16_214104) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "shorties"
+  add_foreign_key "bookings", "users"
   add_foreign_key "shorties", "users"
 end
