58e5658c293c35437219255ac7ada3a9f0e10a71
Jorge Alvarez <jorge.a.alvarez@protonmail.com>
2022-05-17 11:23:58 -0400

    Added some methods in users controller and pages controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,33 @@
 class UsersController < ApplicationController
-  def edit
-  end
+  before_action :set_user
+
+  def show; end
+
+  def edit; end
 
   def update
-  end
-
-  def show
+    if @user.update(restaurant_params)
+      redirect_to @user, notice: 'User information was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @restaurant.destroy
+    redirect_to root, notice: 'User was successfully deleted.'
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+    authorize @user
+  end
+
+  def user_params
+    params.require(:user).permit(:email,
+                                 :first_name,
+                                 :last_name)
   end
 end
