e593ee20ade49ff61e476ca6cf8b77541b74da8f
Jorge Alvarez <jorge.a.alvarez@protonmail.com>
2022-05-17 15:26:55 -0400

    First round of pundit authorizations for testing purposes

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -5,4 +5,22 @@ class UserPolicy < ApplicationPolicy
     #   scope.all
     # end
   end
+
+  def show?
+    true
+  end
+
+  def update?
+    user_is_owner_or_admin
+  end
+
+  def destroy?
+    user_is_owner_or_admin
+  end
+
+  private
+
+  def user_is_owner_or_admin
+    record.user == user || user.admin
+  end
 end
