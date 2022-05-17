ac50605852bb4a55eb33e5eb209089aa142914b8
Jorge Alvarez <jorge.a.alvarez@protonmail.com>
2022-05-17 18:47:42 -0400

    Slight improvements to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+puts "Seeds are being sown..."
+
 Shorty.destroy_all
 User.destroy_all
 Booking.destroy_all
@@ -14,7 +16,7 @@ end
 User.all.each do |user|
   unless user == User.last
     3.times do
-      Shorty.create!(height: rand(150..175),
+      Shorty.create!(height: rand(135..165),
                      name: "#{clones[rand(0..3)]} #{user.first_name}",
                      price: rand(100..500),
                      user: user,
@@ -27,10 +29,11 @@ Shorty.all.each do |shorty|
   other_users = User.where("id != :id", id: shorty.user_id)
   unless shorty == Shorty.first
     2.times do
+      start_date = Faker::Date.between(from: 1.days.from_now, to: 15.days.from_now)
       Booking.create!(shorty: shorty,
                       user: other_users.sample,
-                      start_date: Faker::Date.between(from: 1.days.from_now, to: 2.days.from_now),
-                      end_date: Faker::Date.between(from: 3.days.from_now, to: 4.days.from_now),
+                      start_date: start_date,
+                      end_date: start_date + rand(0..2),
                       status: [0, 1].sample)
     end
   end
