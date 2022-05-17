0e6de88fdb6ba4342fd96b5585126beb4667a8b7
Jorge Alvarez <jorge.a.alvarez@protonmail.com>
2022-05-17 15:35:39 -0400

    Second round of authorizations

--- app/policies/shorty_policy.rb
+++ app/policies/shorty_policy.rb
@@ -5,4 +5,26 @@ class ShortyPolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def show?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    user_is_owner_or_admin
+  end
+
+  def destroy?
+    user_is_owner_or_admin
+  end
+
+  private
+
+  def user_is_owner_or_admin
+    record.user == user || user.admin
+  end
 end
