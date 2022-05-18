260b39c3a5f67a0775a3e746d1816d16f14ff38c
Kim Coquilla <kimcoquilla@gmail.com>
2022-05-18 12:51:55 -0400

    Navbar for profile of current user works now

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,8 @@ clones = ["Black-tie", "Business", "Casual", "Party"]
   User.create!(email: Faker::Internet.email,
                first_name: Faker::Name.first_name,
                last_name: Faker::Name.last_name,
-               password: "123456")
+               password: "123456",
+               photo_url: "https://i.pravatar.cc/1000")
 end
 
 User.all.each do |user|
@@ -25,16 +26,16 @@ User.all.each do |user|
   end
 end
 
-Shorty.all.each do |shorty|
-  other_users = User.where("id != :id", id: shorty.user_id)
-  unless shorty == Shorty.first
-    2.times do
-      start_date = Faker::Date.between(from: 1.days.from_now, to: 15.days.from_now)
-      Booking.create!(shorty: shorty,
-                      user: other_users.sample,
-                      start_date: start_date,
-                      end_date: start_date + rand(0..2),
-                      status: [0, 1].sample)
-    end
-  end
-end
+# Shorty.all.each do |shorty|
+#   other_users = User.where("id != :id", id: shorty.user_id)
+#   unless shorty == Shorty.first
+#     2.times do
+#       start_date = Faker::Date.between(from: 1.days.from_now, to: 15.days.from_now)
+#       # Booking.create!(shorty: shorty,
+#       #                 user: other_users.sample,
+#       #                 start_date: start_date,
+#       #                 end_date: start_date + rand(0..2),
+#       #                 status: [0, 1].sample)
+#     end
+#   end
+# end
