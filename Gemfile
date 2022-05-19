5b078acf3fe7a55e6e720fa5c0400e9ca73c22db
Kim Coquilla <kimcoquilla@gmail.com>
2022-05-19 12:39:41 -0400

    Revamp navbar with PG search

--- Gemfile
+++ Gemfile
@@ -41,6 +41,9 @@ gem 'pundit'
 # Faker
 gem 'faker'
 
+# PG search
+gem 'pg_search', '~> 2.3.0'
+
 group :development, :test do
   gem 'pry-byebug'
   gem 'pry-rails'
--- app/models/shorty.rb
+++ app/models/shorty.rb
@@ -1,4 +1,11 @@
 class Shorty < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
+
+  include PgSearch::Model
+  pg_search_scope :search_by_name_height_price,
+    against: [ :name, :height, :price ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
