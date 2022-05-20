b9ce5d2b58228d881dcf7a7882596df7948624cb
Jorge Alvarez <jorge.a.alvarez@protonmail.com>
2022-05-20 16:50:21 -0400

    Seedsz

--- app/controllers/shorties_controller.rb
+++ app/controllers/shorties_controller.rb
@@ -35,14 +35,12 @@ class ShortiesController < ApplicationController
     @booking = Booking.new
     if @shorty.user == User.find_by(first_name: "Danny")
       image = "danny.png"
-    elsif @shorty.user == User.find_by(first_name: "Kevin")
+    elsif @shorty.user == User.find_by(first_name: "Kevin") || User.find_by(first_name: "Paul") || User.find_by(first_name: "James")
       image = "kevin.png"
     elsif @shorty.user == User.find_by(first_name: "Lionel")
       image = "lionel.png"
     elsif @shorty.user == User.find_by(first_name: "Yann")
       image = "yann.png"
-    else
-      image = "kevin.png"
     end
     @markers = [{
       lat: @shorty.latitude,
@@ -57,7 +55,7 @@ class ShortiesController < ApplicationController
     @shorty.user = current_user
     authorize @shorty
     if @shorty.save
-      redirect_to shorty_path(@shorty), notice: 'Listing was successfully created.'
+      redirect_to shorty_path(@shorty), notice: 'Listing was successfully created!'
     else
       render :new
     end
